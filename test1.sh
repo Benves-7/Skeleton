d463bce529c1019630c1c73a6ba6192ab08c893a
Benjamin Vesterlund <benves-7@student.ltu.se>
2019-09-10 08:59:03 +0200

    complete shell script

--- test1.sh
+++ test1.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 cd projects/assignment-3/code
-# change all .cpp files to .cc files
+
+# change all .cc files to .cpp files
 if ls *.cc >/dev/null 2>&1
     then
         for file in *.cc   
@@ -9,7 +10,7 @@ if ls *.cc >/dev/null 2>&1
                 mv $file ${file%.cc}.cpp
             done
 
-# add copyright to all .cc files
+        # add copyright to all .cc files
         for file in *.cpp
             do 
                 if grep -q "(C)" $file
@@ -19,14 +20,19 @@ if ls *.cc >/dev/null 2>&1
                 cat copyright.copyright $file > temp.temp
                 cat temp.temp > $file
             done
+        # change float to doule
+        for file in *.cpp
+            do
+                sed -i 's/float/double/g' file
+        done
+# change all .cpp files to .cc files
 else
     for file in *.cpp   
         do
             mv $file ${file%.cpp}.cc
         done
 
-# add copyright to all .cc files
-
+        # add copyright to all .cc files
         for file in *.cc
             do 
                 if grep -q "(C)" $file
@@ -36,4 +42,10 @@ else
                 cat copyright.copyright $file > temp.temp
                 cat temp.temp > $file
             done
+        # change double to float
+        for file in *.cpp
+            do
+                sed -i 's/double/float/g' file
+        done
+
 fi
